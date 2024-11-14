57cf0ee9e56cba884d9d39987ce5c4ff619bd0bd
AlexQ63 <alec515@hotmail.fr>
2024-11-14 13:33:44 +0100

    maj feature

--- console.py
+++ console.py
@@ -1,6 +1,19 @@
 import pygame
 
 pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+running = True
 
-pygame.display.set_mode((400,400))
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+#TODOCouleur de l'écran, à voir pour mettre une image 
+    screen.fill("black")
 
+    pygame.display.flip()
+#Limite FPS à 60
+    clock.tick(60)
+
+pygame.quit
